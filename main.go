d5dc673e69927b10afa99988f483d77a3644b861
Rich Stadnick <rich.stadnick@gmail.com>
2021-09-05 19:28:33 -0400

    Boilerplate

--- main.go
+++ main.go
@@ -18,16 +18,15 @@ func Config(key string) string {
 }
 
 func IndexHandler(c *gin.Context) {
-	name := c.Params.ByName("name")
 	c.JSON(200, gin.H{
-		"message": "hello," + name,
+		"message": "Let's Go",
 	})
 }
 
 func main() {
 	port := Config("API_PORT")
 	router := gin.Default()
-	router.GET("/:name", IndexHandler)
+	router.GET("/", IndexHandler)
 
 	router.Run(":" + port)
 }
