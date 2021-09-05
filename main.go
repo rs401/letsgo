cdaab88caa88a40d3efd4c89c3e06b4c94a3074f
Rich Stadnick <rich.stadnick@gmail.com>
2021-09-05 19:48:22 -0400

    recipe data model

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+type Recipe struct {
+	Name         string    `json:"Name"`
+	Tags         []string  `json:"Tags"`
+	Ingredients  []string  `json:"Ingredients"`
+	Instructions []string  `json:"Instructions"`
+	PublishedAt  time.Time `json:"PublishedAt"`
+}
 
 func init() {
 	if err := godotenv.Load(".env"); err != nil {
