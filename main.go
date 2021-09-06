040f98c3c0a01ad339eead5c59c41b78f3ae2a0b
Rich Stadnick <rich.stadnick@gmail.com>
2021-09-06 10:46:20 -0400

    Add Update recipe endpoint

--- main.go
+++ main.go
@@ -60,12 +60,39 @@ func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	recipe.ID = id
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found.",
+		})
+		return
+	}
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	port := Config("API_PORT")
 	r := gin.Default()
 	r.GET("/", IndexHandler)
 	r.POST("/recipes", NewRecipeHandler)
 	r.GET("/recipes", ListRecipesHandler)
+	r.PUT("/recipes/:id", UpdateRecipeHandler)
 
 	r.Run(":" + port)
 }
