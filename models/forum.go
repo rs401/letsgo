9ef08cf70c2fda997aa09b84283a5041629dcc36
Rich Stadnick <rich.stadnick@gmail.com>
2021-09-08 09:21:02 -0400

    Added static files

--- models/forum.go
+++ models/forum.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Forum struct {
 	gorm.Model
 	Name    string `json:"name" gorm:"not null" form:"name"`
-	UserID  uint   `json:"userid"` // gorm:"not null"`
+	UserID  uint   `json:"userid" gorm:"not null"`
 	User    User
 	Threads []Thread
 }
